b0b2b2ebb794f1f7c3f675c11d6384a5d853d3b7
Oskar Puchalski <oskarpuchalski17@gmail.com>
2022-11-17 20:05:52 +0100

    feat: initial commit

---
+++ src/server/trpc/router/example.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+import { router, publicProcedure } from "../trpc";
+
+export const exampleRouter = router({
+  hello: publicProcedure
+    .input(z.object({ name: z.string().nullish() }).nullish())
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input?.name ?? "world"}`,
+      };
+    }),
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.example.findMany();
+  }),
+});
